3a7e1013910f67a3631ecf8870718e1651faacc7
Mashym Ali <mashymalii@gmail.com>
2022-10-25 20:54:06 +0500

    simplify code files

--- model/celebrity_classifier_model.py
+++ model/celebrity_classifier_model.py
@@ -1,29 +1,30 @@
-import numpy as np
 import cv2 as cv
-import matplotlib
-from matplotlib import pyplot as plt
-#%matplotlib inline
+#import matplotlib
+#from matplotlib import pyplot as plt
 import os, shutil
-import pywt
+import pickle
 
 
+#reading original image
 img = cv.imread('model/test/aamir1.jpeg')
 #print(img.shape)
 #plt.imshow(img)
 #plt.show()
 
+#converting orignal image to grayscale
 gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 #print(gray.shape)
 #print(gray)
 #plt.imshow(gray,cmap='gray')
 #plt.show()
 
-
+#calling face and eye features from haarcascades, so we can crop the pictures based on eye features
 face_cascade = cv.CascadeClassifier('model/opencv/haarcascades/haarcascade_frontalface_default.xml')
 eye_cascade = cv.CascadeClassifier('model/opencv/haarcascades/haarcascade_eye.xml')
 
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-#print(faces)
+#print(faces)  #return [220  31 293 293]  means x,y width,height where face lies in picture 
+
 
 (x,y,w,h) = faces[0]  
 #print(x,y,w,h)  #x-axis , y-axis , width , height
@@ -32,21 +33,7 @@ face_img = cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)  #prints rectangle arou
 #plt.imshow(face_img)
 #plt.show()
 
-
-#for (x,y,w,h) in faces:
-#    face_img = cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-#    roi_gray = gray[y:y+h, x:x+w]
-#    roi_color = face_img[y:y+h, x:x+w]
-#    eyes = eye_cascade.detectMultiScale(roi_gray)
-#    for (ex,ey,ew,eh) in eyes:
-#        cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-        
-
-#plt.figure()
-#plt.imshow(face_img, cmap='gray') #show whole image
-#plt.imshow(roi_color, cmap='gray') #show cropped face
-#plt.show()
-
+#function that will give cropped images when it can detect two eyes in it
 def get_cropped_image_if_2_eyes(image_path):
     img = cv.imread(image_path)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -87,7 +74,7 @@ img_dirs = []
 for entry in os.scandir(path_to_data):
     if entry.is_dir():
         img_dirs.append(entry.path)
-print(img_dirs)  #print array/list of all folder names in dataset folder
+#print(img_dirs)  #print array/list of all folder names in dataset folder
 
 
 #creating and removing cropped folder
@@ -123,161 +110,8 @@ for img_dir in img_dirs:
             count += 1  
 
 
-# Preprocessing: Use wavelet transform as a feature for traning our model
-def w2d(img, mode='haar', level=1):
-    imArray = img
-    #Datatype conversions
-    #convert to grayscale
-    imArray = cv.cvtColor( imArray,cv.COLOR_RGB2GRAY )
-    #convert to float
-    imArray =  np.float32(imArray)   
-    imArray /= 255
-    # compute coefficients 
-    coeffs=pywt.wavedec2(imArray, mode, level=level)
-
-    #Process Coefficients
-    coeffs_H=list(coeffs)  
-    coeffs_H[0] *= 0;  
-
-    # reconstruction
-    imArray_H=pywt.waverec2(coeffs_H, mode)
-    imArray_H *= 255
-    imArray_H =  np.uint8(imArray_H)
-
-    return imArray_H
-
-
-#im_har = w2d(cropped_image,'db1',5)
-#plt.imshow(im_har, cmap='gray')
-#plt.show()
-
-
-print(celebrity_file_names_dict)
-
-class_dict = {}
-count = 0
-for celebrity_name in celebrity_file_names_dict.keys():
-    class_dict[celebrity_name] = count
-    count = count + 1
-print(class_dict)
-
-
-X, y = [], []
-for celebrity_name, training_files in celebrity_file_names_dict.items():
-    for training_image in training_files:
-        img = cv.imread(training_image)
-        if img is None:
-            continue
-        scalled_raw_img = cv.resize(img, (32, 32))
-        img_har = w2d(img,'db1',5)
-        scalled_img_har = cv.resize(img_har, (32, 32))
-        combined_img = np.vstack((scalled_raw_img.reshape(32*32*3,1),scalled_img_har.reshape(32*32,1)))
-        X.append(combined_img)
-        y.append(class_dict[celebrity_name]) 
-
-
-print(len(X))
-X = np.array(X).reshape(len(X),4096).astype(float)
-print(X.shape)
-
-
-# DATA CLEANING IS DONE AT THIS POINT
-
-# TRAIN MODEL STARTING
-
-from sklearn.svm import SVC
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
-from sklearn.metrics import classification_report
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-
-pipe = Pipeline([('scaler', StandardScaler()), ('svc', SVC(kernel = 'rbf', C = 10))])
-pipe.fit(X_train, y_train)
-print("Score: ",pipe.score(X_test, y_test))
-print("Total images",len(X_test))
-
-print("\n")
-print(classification_report(y_test, pipe.predict(X_test)))
-
-from sklearn import svm
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import GridSearchCV
-
-model_params = {
-    'svm': {
-        'model': svm.SVC(gamma='auto',probability=True),
-        'params' : {
-            'svc__C': [1,10,100,1000],
-            'svc__kernel': ['rbf','linear']
-        }  
-    },
-    'random_forest': {
-        'model': RandomForestClassifier(),
-        'params' : {
-            'randomforestclassifier__n_estimators': [1,5,10]
-        }
-    },
-    'logistic_regression' : {
-        'model': LogisticRegression(solver='liblinear',multi_class='auto'),
-        'params': {
-            'logisticregression__C': [1,5,10]
-        }
-    }
-}
-
-print("done1")
-
-scores = []
-best_estimators = {}
-import pandas as pd
-for algo, mp in model_params.items():
-    pipe = make_pipeline(StandardScaler(), mp['model'])
-    clf =  GridSearchCV(pipe, mp['params'], cv=5, return_train_score=False)
-    clf.fit(X_train, y_train)
-    scores.append({
-        'model': algo,
-        'best_score': clf.best_score_,
-        'best_params': clf.best_params_
-    })
-    best_estimators[algo] = clf.best_estimator_
-
-print("done2")
-    
-print("\n")
-df = pd.DataFrame(scores,columns=['model','best_score','best_params'])
-print(df)
-print("done3")
-
-print("\n")
-print(best_estimators)
-
-print("\n")
-print("SVM: ",best_estimators['svm'].score(X_test,y_test))
-print("Random Forest: ",best_estimators['random_forest'].score(X_test,y_test))
-print("logistic regression: ",best_estimators['logistic_regression'].score(X_test,y_test))
-best_clf = best_estimators['svm']
-print("done4")
-
-print("\n")
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, best_clf.predict(X_test))
-print(cm)
-
-print("\n")
-print(class_dict)
-
-
-import joblib 
-# Save the model as a pickle in a file 
-joblib.dump(best_clf, 'saved_model.pkl') 
-print("done pickling")
-
-import json
-with open("class_dictionary.json","w") as f:
-    f.write(json.dumps(class_dict))
-print("done json file")
-
+#pickling this dictionary so we can access this into our training_model file
+file = "celebrity_file_names_paths_dict.pkl"
+fileobj = open(file, 'wb')
+pickle.dump(celebrity_file_names_dict, fileobj)
+fileobj.close()
---
+++ model/training_model.py
@@ -0,0 +1,155 @@
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report
+import cv2 as cv
+import pickle
+import numpy as np
+import pywt
+
+
+
+file = "celebrity_file_names_paths_dict.pkl"
+fileobj = open(file, 'rb')
+celebrity_file_names_dict = pickle.load(fileobj)
+
+# Preprocessing: Use wavelet transform as a feature for traning our model
+def w2d(img, mode='haar', level=1):
+    imArray = img
+    #Datatype conversions
+    #convert to grayscale
+    imArray = cv.cvtColor( imArray,cv.COLOR_RGB2GRAY )
+    #convert to float
+    imArray =  np.float32(imArray)   
+    imArray /= 255
+    # compute coefficients 
+    coeffs=pywt.wavedec2(imArray, mode, level=level)
+
+    #Process Coefficients
+    coeffs_H=list(coeffs)  
+    coeffs_H[0] *= 0;  
+
+    # reconstruction
+    imArray_H=pywt.waverec2(coeffs_H, mode)
+    imArray_H *= 255
+    imArray_H =  np.uint8(imArray_H)
+
+    return imArray_H
+
+
+
+
+class_dict = {}
+count = 0
+for celebrity_name in celebrity_file_names_dict.keys():  #provides 0 till 99 index to each folder basically
+    class_dict[celebrity_name] = count
+    count = count + 1
+#print(class_dict)
+
+
+
+X, y = [], []
+for celebrity_name, training_files in celebrity_file_names_dict.items():
+    for training_image in training_files:
+        img = cv.imread(training_image)
+        if img is None:
+            continue
+        scalled_raw_img = cv.resize(img, (32, 32))
+        img_har = w2d(img,'db1',5)
+        scalled_img_har = cv.resize(img_har, (32, 32))
+        combined_img = np.vstack((scalled_raw_img.reshape(32*32*3,1),scalled_img_har.reshape(32*32,1)))
+        X.append(combined_img)
+        y.append(class_dict[celebrity_name]) 
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+#pipe = Pipeline([('scaler', StandardScaler()), ('svc', SVC())])
+pipe = Pipeline([('scaler', StandardScaler()), ('svc', SVC(kernel = 'rbf', C = 10))])
+pipe.fit(X_train, y_train)
+#print("Score: ",pipe.score(X_test, y_test))
+#print("Total images",len(X_test))
+
+#print("\n")
+#print(classification_report(y_test, pipe.predict(X_test)))
+#
+#from sklearn import svm
+#from sklearn.ensemble import RandomForestClassifier
+#from sklearn.linear_model import LogisticRegression
+#from sklearn.pipeline import make_pipeline
+#from sklearn.model_selection import GridSearchCV
+#
+#model_params = {
+#    'svm': {
+#        'model': svm.SVC(gamma='auto',probability=True),
+#        'params' : {
+#            'svc__C': [1,10,100,1000],
+#            'svc__kernel': ['rbf','linear']
+#        }  
+#    },
+#    'random_forest': {
+#        'model': RandomForestClassifier(),
+#        'params' : {
+#            'randomforestclassifier__n_estimators': [1,5,10]
+#        }
+#    },
+#    'logistic_regression' : {
+#        'model': LogisticRegression(solver='liblinear',multi_class='auto'),
+#        'params': {
+#            'logisticregression__C': [1,5,10]
+#        }
+#    }
+#}
+#
+#print("done1")
+#
+#scores = []
+#best_estimators = {}
+#import pandas as pd
+#for algo, mp in model_params.items():
+#    pipe = make_pipeline(StandardScaler(), mp['model'])
+#    clf =  GridSearchCV(pipe, mp['params'], cv=5, return_train_score=False)
+#    clf.fit(X_train, y_train)
+#    scores.append({
+#        'model': algo,
+#        'best_score': clf.best_score_,
+#        'best_params': clf.best_params_
+#    })
+#    best_estimators[algo] = clf.best_estimator_
+#
+#print("done2")
+#    
+#print("\n")
+#df = pd.DataFrame(scores,columns=['model','best_score','best_params'])
+#print(df)
+#print("done3")
+#
+#print("\n")
+#print(best_estimators)
+#
+#print("\n")
+#print("SVM: ",best_estimators['svm'].score(X_test,y_test))
+#print("Random Forest: ",best_estimators['random_forest'].score(X_test,y_test))
+#print("logistic regression: ",best_estimators['logistic_regression'].score(X_test,y_test))
+#best_clf = best_estimators['svm']
+#print("done4")
+#
+#print("\n")
+#from sklearn.metrics import confusion_matrix
+#cm = confusion_matrix(y_test, best_clf.predict(X_test))
+#print(cm)
+#
+#print("\n")
+#print(class_dict)
+#
+#
+#import joblib 
+## Save the model as a pickle in a file 
+#joblib.dump(best_clf, 'saved_model.pkl') 
+#print("done pickling")
+#
+#import json
+#with open("class_dictionary.json","w") as f:
+#    f.write(json.dumps(class_dict))
+#print("done json file")
