a218daaf2be41b4efb274fe1ef4d210bf60b7ccb
Mashym Ali <mashymalii@gmail.com>
2022-10-31 20:10:13 +0500

    model is trained

--- Readme.md
+++ Readme.md
@@ -1,16 +1,17 @@
 celebrity_classifier_model.py is where data cleaning is actually happening</br>
-training_model.py is a new file where model will be trained</br>
-Data is cleaned</br>
-All cropped and cleaned pictures of celebs are in cropped folder inside dataset folder</br>
-All libraries used till today are in rquirements.txt file </br>
-test folder is unwanted, just there to explain some starting code in celebrity_classifier_model.py file.</br>
-make sure to delete cropped folder and .pckl file on first run so you can actually see the data cleaning happening.</br>
-you can delete all the folders except cropped folder inside the dataset folder which is inside model folder, after ensuring that data cleaning is happening after first run</br>
-
-
 </br>
+training_model.py is a new file where model is being trained</br>
+</br>
+All cropped and cleaned pictures of celebs are in cropped folder inside dataset folder</br>
+</br>
+All libraries used till today are in requirements.txt file </br>
+</br>
+make sure to delete cropped folder and .pckl and .json files on first run so you can actually see the data cleaning and model training happening.</br>
+
+</br> <b> Model used: </b> Logistic regression. Accuracy while training is 78 and while testing is 84
+</br>
+
 <b> Still pending</b> </br>
-Model training </br>
 Deploying to flask </br>
 
 
--- model/training_model.py
+++ model/training_model.py
@@ -1,13 +1,14 @@
-from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
-from sklearn.metrics import classification_report
 import cv2 as cv
 import pickle
 import numpy as np
 import pywt
-
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import svm
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import GridSearchCV
 
 
 file = "celebrity_file_names_paths_dict.pkl"
@@ -45,7 +46,7 @@ count = 0
 for celebrity_name in celebrity_file_names_dict.keys():  #provides 0 till 99 index to each folder basically
     class_dict[celebrity_name] = count
     count = count + 1
-#print(class_dict)
+print(class_dict)
 
 
 
@@ -65,29 +66,12 @@ for celebrity_name, training_files in celebrity_file_names_dict.items():
 X = np.array(X).reshape(len(X),4096).astype(float)
 print(X.shape)  #prints (2212, 4096) which means (no.of images, pic is represented as 1d array)
 
-# THIS IS NOT WORKING (▱˘︹˘▱)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-#pipe = Pipeline([('scaler', StandardScaler()), ('svc', SVC())])
-#pipe = Pipeline([('scaler', StandardScaler()), ('svc', SVC(kernel = 'rbf', C = 1000))]) #creating svm model
-#pipe.fit(X_train, y_train)  #training model created above
-#print("Score: ",pipe.score(X_test, y_test))
-#print("Total images tested: ",len(X_test))
 
-#We cannot use SVC anymore as its not giving much accuracy (▱˘︹˘▱). 
-#I have searched a bit and found that svc is not much suitable for large datasets 😭😭
-
-#print("\n")
-#print(classification_report(y_test, pipe.predict(X_test)))
-#
 
 # TRYING DIFFERENT ALGOS FOR ACCURACY
-from sklearn import svm
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import GridSearchCV
-#
+
 model_params = {
    'svm': {
         'model': svm.SVC(gamma='auto',probability=True),
@@ -110,14 +94,13 @@ model_params = {
     }
 }
 
-print("done1")
 
 scores = []
 best_estimators = {}
 import pandas as pd
 for algo, mp in model_params.items():
     pipe = make_pipeline(StandardScaler(), mp['model'])
-    clf =  GridSearchCV(pipe, mp['params'], cv=3, return_train_score=False)
+    clf =  GridSearchCV(pipe, mp['params'], cv=5, return_train_score=False)
     clf.fit(X_train, y_train)
     scores.append({
         'model': algo,
@@ -125,39 +108,37 @@ for algo, mp in model_params.items():
         'best_params': clf.best_params_
     })
     best_estimators[algo] = clf.best_estimator_
+    print(f'iteration {count}')
+    count+=1
 
-#print("done2")
-#    
-#print("\n")
+    
+print("\n")
 df = pd.DataFrame(scores,columns=['model','best_score','best_params'])
 print(df)
-#print("done3")
-#
-#print("\n")
-#print(best_estimators)
-#
-#print("\n")
-#print("SVM: ",best_estimators['svm'].score(X_test,y_test))
-#print("Random Forest: ",best_estimators['random_forest'].score(X_test,y_test))
-#print("logistic regression: ",best_estimators['logistic_regression'].score(X_test,y_test))
-#best_clf = best_estimators['svm']
-#print("done4")
-#
-#print("\n")
-#from sklearn.metrics import confusion_matrix
-#cm = confusion_matrix(y_test, best_clf.predict(X_test))
-#print(cm)
-#
-#print("\n")
-#print(class_dict)
-#
-#
-#import joblib 
-## Save the model as a pickle in a file 
-#joblib.dump(best_clf, 'saved_model.pkl') 
-#print("done pickling")
-#
-#import json
-#with open("class_dictionary.json","w") as f:
-#    f.write(json.dumps(class_dict))
-#print("done json file")
+
+print(best_estimators)
+
+
+print("\n Accuracy with test data: ")
+print("SVM: ",best_estimators['svm'].score(X_test,y_test))
+print("Random Forest: ",best_estimators['random_forest'].score(X_test,y_test))
+print("logistic regression: ",best_estimators['logistic_regression'].score(X_test,y_test))
+best_clf = best_estimators['logistic_regression']
+
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, best_clf.predict(X_test))
+print(cm)
+
+print(class_dict)
+
+
+import joblib 
+# Save the model as a pickle in a file 
+joblib.dump(best_clf, 'saved_model.pkl') 
+print("done pickling")
+
+import json
+with open("class_dictionary.json","w") as f:
+    f.write(json.dumps(class_dict))
+print("done json file")
