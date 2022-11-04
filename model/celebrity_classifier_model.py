8fcf8a2ca7d0429d710ef2bc790c120421a3fc96
Mashym Ali <mashymalii@gmail.com>
2022-11-05 00:15:44 +0500

    random commit

--- model/celebrity_classifier_model.py
+++ model/celebrity_classifier_model.py
@@ -1,37 +1,13 @@
 import cv2 as cv
-#import matplotlib
-#from matplotlib import pyplot as plt
 import os, shutil
 import pickle
 
 
-#reading original image
-#img = cv.imread('model/test/aamir1.jpeg')
-#print(img.shape)
-#plt.imshow(img)
-#plt.show()
-
-#converting orignal image to grayscale
-#gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-#print(gray.shape)
-#print(gray)
-#plt.imshow(gray,cmap='gray')
-#plt.show()
 
 #calling face and eye features from haarcascades, so we can crop the pictures based on eye features
 face_cascade = cv.CascadeClassifier('model/opencv/haarcascades/haarcascade_frontalface_default.xml')
 eye_cascade = cv.CascadeClassifier('model/opencv/haarcascades/haarcascade_eye.xml')
 
-#faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-#print(faces)  #return [220  31 293 293]  means x,y width,height where face lies in picture 
-
-
-#(x,y,w,h) = faces[0]  
-#print(x,y,w,h)  #x-axis , y-axis , width , height
-
-#face_img = cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)  #prints rectangle around the face
-#plt.imshow(face_img)
-#plt.show()
 
 #function that will give cropped images when it can detect two eyes in it
 def get_cropped_image_if_2_eyes(image_path):
@@ -44,25 +20,6 @@ def get_cropped_image_if_2_eyes(image_path):
         eyes = eye_cascade.detectMultiScale(roi_gray)
         if len(eyes) >= 2:
             return roi_color
-
-#original image with two eyes
-#original_image = cv.imread('model/test/aamir1.jpeg')
-#plt.imshow(original_image)
-#plt.show()
-
-#cropped image with two eyes
-#cropped_image = get_cropped_image_if_2_eyes('./model/test/aamir1.jpeg')
-#plt.imshow(cropped_image)
-#plt.show()
-
-#original image without two eyes
-#original_image_obstructed = cv.imread('model/test/aamir3.jpg')
-#plt.imshow(original_image_obstructed)
-#plt.show()
-
-#cropped image without two eyes
-#cropped_image_no_2_eyes = get_cropped_image_if_2_eyes('model/test/aamir3.jpg')
-#print(cropped_image_no_2_eyes) #prints None because this picture has no 2 eyes
 
 
 #store cropped images in cropped folder inside dataset folder
